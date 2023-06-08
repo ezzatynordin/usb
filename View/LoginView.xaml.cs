bfd4aec13790fc55706a3d8cb7fa6df3acf383ef
ezzaty <nurezzatynordin@gmail.com>
2023-06-08 15:43:07 +0800

    login

--- View/LoginView.xaml.cs
+++ View/LoginView.xaml.cs
@@ -39,5 +39,10 @@ namespace usb.View
         {
             Application.Current.Shutdown();
         }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
