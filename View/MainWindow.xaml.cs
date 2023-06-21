602a136a6a05db5fb025fb9b0adedacd6f6b869f
ezzaty <nurezzatynordin@gmail.com>
2023-06-21 17:34:07 +0800

    update

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace usb.View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow: Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
@@ -35,12 +35,54 @@ namespace usb.View
         {
             // Navigate to the USB List page
             ContentFrame.Navigate(new USBList());
- 
         }
+
         private void ListViewItem1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            // Navigate to the USB List page
+            // Navigate to the Activity Log page
             ContentFrame.Navigate(new ActivityLog());
+        }
+
+        private void ListViewItem2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // Display a message box and handle the user's response
+            var result = MessageBox.Show("Please plug in the USB to detect.", "USB Detection", MessageBoxButton.OKCancel);
+
+            if (result == MessageBoxResult.OK)
+            {
+                // User clicked OK, proceed with the USB detection
+                ContentFrame.Navigate(new DetectNewUSB());
+            }
+            else
+            {
+                // User clicked Cancel or closed the message box, stop the activity
+                // Add code here to handle the activity stoppage
+                StopActivity();
+            }
+        }
+
+        private void StopActivity()
+        {
+            // Add code here to handle the activity stoppage
+
+            // For example, you can perform cleanup tasks, reset variables, or stop ongoing processes
+            // Stop any ongoing USB detection processes or timers
+            // Reset the application state to the initial state
+
+            // Display a message or perform any necessary cleanup
+            MessageBox.Show("Activity stopped.", "USB Detection", MessageBoxButton.OK);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the application
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Minimize the window
+            WindowState = WindowState.Minimized;
         }
 
         private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
